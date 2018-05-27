28f78c731212c7c1ba134fa599a211ed78ed03fb
Mehreen Butt <mbutt@republiccharterschools.org>
2018-05-27 18:43:47 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board
-  puts "   |"
+  puts "   |   |   "
+  puts "___________"
+  puts "   |   |   "
+  puts "___________"
+  puts "   |   |   "
+end
