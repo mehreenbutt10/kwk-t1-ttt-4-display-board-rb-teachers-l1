f42b63e7aef2673650d0306b895878664a17bc29
Mehreen Butt <mbutt@republiccharterschools.org>
2018-05-27 18:57:09 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -8,6 +8,6 @@ def display_board(board)
   puts "#{board[6]}|#{board[7]}|#{board[8]}"
 end
 
-board = [" X ", " 0 ", " 0 ", " X ", " X ", " 0 ", " 0 ", " X ", " 0 "]
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 display_board(board)
